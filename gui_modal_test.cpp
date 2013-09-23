e5d033d9f415d724a45cf7034aa2ae6cc8ecf9f8
Michael Zeilfelder <info@michaelzeilfelder.de>
2013-09-23 22:52:14 +0200

    Add serialization tests for modal dialogs and windows.

---
+++ gui_modal_test.cpp
@@ -0,0 +1,69 @@
+// Code is under the zlib license (same as Irrlicht)
+// Written by Michael Zeilfelder
+// 
+// Serialization tests for modal dialogs.
+
+#include <irrlicht.h>
+#include <iostream>
+
+using namespace irr;
+using namespace core;
+using namespace scene;
+using namespace video;
+using namespace io;
+using namespace gui;
+
+#ifdef _IRR_WINDOWS_
+#pragma comment(lib, "Irrlicht.lib")
+#endif
+
+
+int main()
+{
+	video::E_DRIVER_TYPE driverType = video::EDT_OPENGL;
+	IrrlichtDevice * device = createDevice(driverType, core::dimension2d<u32>(640, 480));
+	if (device == 0)
+		return 1; // could not create selected driver.
+
+	video::IVideoDriver* driver = device->getVideoDriver();
+	IGUIEnvironment* env = device->getGUIEnvironment();
+
+	s32 top = 10;
+	s32 default_height = 100;
+	s32 default_width = 180;
+	s32 default_gap = 30;
+	core::rect<s32> rect(10, top, 10 + default_width, top + default_height);
+
+	IGUIWindow * wndParent = env->addWindow (core::rect<s32>(100, 100, 300, 300), false, L"", 0, -1);
+	
+	IGUIWindow * wnd = NULL;
+	wnd = env->addWindow (rect, /*modal*/true, L"child_window", wndParent, -1);
+
+	rect.UpperLeftCorner.Y = rect.LowerRightCorner.Y + default_gap;
+	rect.LowerRightCorner.Y = rect.UpperLeftCorner.Y + default_height;
+	wnd = env->addWindow (rect, /*modal*/true, L"text_window2", NULL, -1);
+	wnd->setVisible(false);
+
+	env->addMessageBox(L"message box", L"I am modal", true, EMBF_OK);
+
+	env->saveGUI ("wnd_gui.xml");
+	env->clear();
+	env->loadGUI ("wnd_gui.xml");
+
+	while(device->run() && driver)
+	{
+		if (device->isWindowActive())
+		{
+			driver->beginScene(true, true, SColor(0,200,200,200));
+
+			env->drawAll();
+
+			driver->endScene();
+		}
+	}
+
+	device->drop();
+
+	return 0;
+}
+
---
+++ gui_serialize_window.cpp
@@ -0,0 +1,76 @@
+// Code is under the zlib license (same as Irrlicht)
+// Written by Michael Zeilfelder
+// 
+// serialization tests for windows.
+
+#include <irrlicht.h>
+#include <iostream>
+
+using namespace irr;
+using namespace core;
+using namespace scene;
+using namespace video;
+using namespace io;
+using namespace gui;
+
+#ifdef _IRR_WINDOWS_
+#pragma comment(lib, "Irrlicht.lib")
+#endif
+
+
+int main()
+{
+	video::E_DRIVER_TYPE driverType = video::EDT_OPENGL;
+	IrrlichtDevice * device = createDevice(driverType, core::dimension2d<u32>(640, 480));
+	if (device == 0)
+		return 1; // could not create selected driver.
+
+	video::IVideoDriver* driver = device->getVideoDriver();
+	IGUIEnvironment* env = device->getGUIEnvironment();
+	
+	s32 top = 50;
+	s32 default_height = 100;
+	s32 default_width = 180;
+	s32 default_gap = 30;
+	core::rect<s32> rect(10, top, 10 + default_width, top + default_height);
+
+	IGUIWindow * wnd = NULL;
+	wnd = env->addWindow (rect, false, L"text_window", NULL, -1);
+	wnd->setDraggable(true);
+
+	rect.UpperLeftCorner.Y = rect.LowerRightCorner.Y + default_gap;
+	rect.LowerRightCorner.Y = rect.UpperLeftCorner.Y + default_height;
+	wnd = env->addWindow (rect, false, L"text_window", NULL, -1);
+	wnd->getCloseButton()->setVisible(false);
+	wnd->setDraggable(false);
+	wnd->setDrawTitlebar(false);
+
+	rect.UpperLeftCorner.Y = top;
+	rect.UpperLeftCorner.X += default_width + default_gap;
+	rect.LowerRightCorner.Y = rect.UpperLeftCorner.Y + default_height;
+	rect.LowerRightCorner.X = rect.UpperLeftCorner.X + default_width;
+	wnd = env->addWindow (rect, false, L"text_window",NULL, -1);
+	wnd->setDraggable(true);
+	wnd->setDrawBackground(false);
+	
+	env->saveGUI ("wnd_gui.xml");
+	env->clear();
+	env->loadGUI ("wnd_gui.xml");
+
+	while(device->run() && driver)
+	{
+		if (device->isWindowActive())
+		{
+			driver->beginScene(true, true, SColor(0,200,200,200));
+	
+			env->drawAll();
+		
+			driver->endScene();
+		}
+	}
+
+	device->drop();
+
+	return 0;
+}
+
