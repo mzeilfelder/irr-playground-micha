175513f950ea3ef72c934cbb072b11a5c1e35df7
Michael Zeilfelder <info@michaelzeilfelder.de>
2014-02-13 00:12:05 +0100

    Add a test to load a (xml) font.

---
+++ gui_font.cpp
@@ -0,0 +1,54 @@
+// Code is under the zlib license (same as Irrlicht)
+// Written by Michael Zeilfelder
+// 
+// Test loading an xml font.
+
+#include <irrlicht.h>
+#include <iostream>
+
+using namespace irr;
+using namespace core;
+using namespace scene;
+using namespace video;
+using namespace io;
+using namespace gui;
+
+#ifdef _IRR_WINDOWS_
+#pragma comment(lib, "Irrlicht.lib")
+#endif
+
+
+int main()
+{
+	video::E_DRIVER_TYPE driverType = video::EDT_OPENGL;
+	IrrlichtDevice * device = createDevice(driverType, core::dimension2d<u32>(640, 480));
+	if (device == 0)
+		return 1; // could not create selected driver.
+
+	video::IVideoDriver* driver = device->getVideoDriver();
+	IGUIEnvironment* env = device->getGUIEnvironment();
+	
+	IGUISkin* skin = env->getSkin();
+	IGUIFont* font = env->getFont("media_foo/fonts/droid_sans12.xml");
+	if (font)
+		skin->setFont(font);	
+	
+	env->addStaticText(L"The quick brown fox jumps over the lazy dog", core::rect<s32>(10,10, 400, 200), true, true);
+
+	while(device->run() && driver)
+	{
+		if (device->isWindowActive())
+		{
+			driver->beginScene(true, true, SColor(0,200,200,200));
+	
+			env->drawAll();
+		
+			driver->endScene();
+		}
+	}
+
+	device->drop();
+
+	return 0;
+}
+
