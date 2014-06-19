6f8436b170905d59e330e5d9e4d02c061ae3d197
Michael Zeilfelder <info@michaelzeilfelder.de>
2014-06-19 22:29:15 +0200

    angle vs dot-product (after a few hours coding I start mixing up plus and minus without helper)

---
+++ angles.cpp
@@ -0,0 +1,41 @@
+// Code is under the zlib license (same as Irrlicht)
+// Written by Michael Zeilfelder
+//
+// For those tired nights when you can no longer can remember the the value ranges.
+
+#include <irrlicht.h>
+#include <iostream>
+
+using namespace irr;
+using namespace core;
+using namespace scene;
+using namespace video;
+using namespace io;
+using namespace gui;
+
+#ifdef _IRR_WINDOWS_
+#pragma comment(lib, "Irrlicht.lib")
+#endif
+
+
+int main()
+{	
+	vector2df a(0.f, 1.f);
+	
+	array<vector2df> t;
+	t.push_back(vector2df(0.f, 1.f));
+	t.push_back(vector2df(0.f, -1.f));
+	t.push_back(vector2df(1.f, 0.f));
+	t.push_back(vector2df(-1.f, 0.f));
+	t.push_back(vector2df(0.707106781187f, 0.707106781187f));
+	t.push_back(vector2df(0.707106781187f, -0.707106781187f));
+	t.push_back(vector2df(-0.707106781187f, 0.707106781187f));
+	t.push_back(vector2df(-0.707106781187f, -0.707106781187f));
+	
+	for (u32 i=0; i< t.size(); ++i )
+	{
+		float angle = atan2(t[i].Y, t[i].X) - atan2(a.Y, a.X);		
+		std::cout << "(" << t[i].X << "," << t[i].Y << "): " << a.dotProduct(t[i]) << " a: " << angle << "\n";
+	}
+	return 0;
+}
