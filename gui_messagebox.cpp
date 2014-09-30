1d2a2f88267dee768655a18be185399b2243ffbc
Michael Zeilfelder <info@michaelzeilfelder.de>
2014-09-30 22:20:14 +0200

    Fix texture path.

--- gui_messagebox.cpp
+++ gui_messagebox.cpp
@@ -10,13 +10,13 @@ using namespace irr;
 
 void AddMessageBoxes(video::IVideoDriver* driver, gui::IGUIEnvironment* env)
 {
-	video::ITexture * tex64 = driver->getTexture("64x64.jpg");
+	video::ITexture * tex64 = driver->getTexture("my_media/64x64.jpg");
 	env->addMessageBox (L"", L"", /*modal=*/false, /*s32 flags=*/gui::EMBF_OK, /*IGUIElement *parent=*/0, /*s32 id=*/-1, tex64);
 	env->addMessageBox (L"Caption", L"text.", /*modal=*/false, /*s32 flags=*/gui::EMBF_OK, /*IGUIElement *parent=*/0, /*s32 id=*/-1, tex64);
 	env->addMessageBox (L"Caption", L"text.\nLine two\n. Line looooooooooooooooooooooooooooooooooooooooooooong it still gooooooooes on and on and on and on!", /*modal=*/false, /*s32 flags=*/gui::EMBF_OK | gui::EMBF_YES | gui::EMBF_NO, /*IGUIElement *parent=*/0, /*s32 id=*/-1, tex64);
 	env->addMessageBox (L"Caption", L"text many lines.\nLine \nLine \nLine \nLine \nLine \nLine \nLine \nLine \nLine \nLine \nLine \nLine \nLine \nLine \nLine \nLine \nLine \nLine \nLine \nLine \nlast line", /*modal=*/false, /*s32 flags=*/gui::EMBF_OK, /*IGUIElement *parent=*/0, /*s32 id=*/-1, tex64);	
 	
-	video::ITexture * tex420 = driver->getTexture("420x300.jpg");
+	video::ITexture * tex420 = driver->getTexture("my_media/420x300.jpg");
 	env->addMessageBox (L"Caption", L"text.\nLine two\n. Line three.", /*modal=*/false, /*s32 flags=*/gui::EMBF_OK, /*IGUIElement *parent=*/0, /*s32 id=*/-1, tex420);
 	
 	env->addMessageBox (L"Caption", L"text.", /*modal=*/false, /*s32 flags=*/gui::EMBF_OK, /*IGUIElement *parent=*/0, /*s32 id=*/-1, 0);	
