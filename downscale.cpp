5d9260b8f4bcac66da8c9523036b5e4202a96a8f
Michael Zeilfelder <info@michaelzeilfelder.de>
2014-12-18 18:30:56 +0100

    Add an example for downscaling a texture.

---
+++ downscale.cpp
@@ -0,0 +1,65 @@
+// Code is under the zlib license (same as Irrlicht)
+// Written by Michael Zeilfelder
+//
+// Downscale a texture
+
+#include <irrlicht.h>
+#include "driverChoice.h"
+
+#ifdef _IRR_WINDOWS_
+#pragma comment(lib, "Irrlicht.lib")
+#endif
+
+using namespace irr;
+
+
+irr::video::ITexture* downscaleTexture(irr::video::IVideoDriver* videoDriver, irr::video::ITexture* texture, irr::u32 textureDivider)
+{
+	const irr::core::dimension2d<irr::u32>& texSize = texture->getSize();
+	texture->grab();
+	videoDriver->removeTexture(texture);
+	irr::video::IImage* image = videoDriver->createImage (texture, irr::core::position2d<irr::s32>(0,0), texSize);
+	irr::video::IImage* imageSmall = videoDriver->createImage(texture->getColorFormat(), texSize/textureDivider);
+	image->copyToScaling(imageSmall);
+	irr::video::ITexture* texSmall = videoDriver->addTexture(texture->getName(), imageSmall);
+	image->drop();
+	imageSmall->drop();
+	texture->drop();
+
+	return texSmall;
+}
+
+
+int main()
+{
+	irr::SIrrlichtCreationParameters params;
+	params.DriverType = driverChoiceConsole();
+	params.WindowSize = core::dimension2d<u32>(800, 600);
+	IrrlichtDevice* device = createDeviceEx(params);
+
+	if (!device)
+		return 1;
+
+	video::IVideoDriver* driver = device->getVideoDriver();
+	gui::IGUIEnvironment* env = device->getGUIEnvironment();
+
+	video::ITexture* texture = driver->getTexture("my_media/1024x1024.jpg");
+	texture = downscaleTexture(driver, texture, 2);
+
+	env->addImage(texture, core::position2d<s32>(10, 10));
+
+	while (device->run())
+	{
+		if (device->isWindowActive())
+		{
+			driver->beginScene(true, true, video::SColor(255, 255, 255, 255));
+
+			env->drawAll();
+
+			driver->endScene();
+		}
+	}
+
+	device->drop();
+	return 0;
+}
