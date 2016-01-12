a6d4a649145573eb45c9b3caee6fba1d3718b4f4
Michael Zeilfelder <info@michaelzeilfelder.de>
2016-01-12 21:09:51 +0100

    Update randomizer test.

--- randomizer.cpp
+++ randomizer.cpp
@@ -1,6 +1,6 @@
 // Code is under the zlib license (same as Irrlicht)
 // Written by Michael Zeilfelder
-// 
+//
 // Test for a bug in the random generator reported by AndreeeCZ in #irrlicht IRC channel.
 
 #include <irrlicht.h>
@@ -12,15 +12,34 @@ int main(int argc, char *argv[])
 {
 	IrrlichtDevice * device = createDevice(video::EDT_NULL);
 	IRandomizer* randomizer = device->getRandomizer ();
+
+	for ( int s = -3; s <= 3; ++ s )
+	{
+		randomizer->reset(s);
+		std::cout << s << "." << std::endl;
+		for ( int i=0; i<10; ++i)
+			std::cout << randomizer->frand() << std::endl; // zeroes all the time
+
+	}
 	
+	randomizer->reset(2147483647);
+	std::cout << "with 2147483647" << std::endl;
 	for ( int i=0; i<10; ++i)
 		std::cout << randomizer->frand() << std::endl; // zeroes all the time
 	
-	// After resetting we only get 0's below - before it works.
-	randomizer->reset((s32) device->getTimer()->getTime());
-	
+	randomizer->reset(2147483648);
+	std::cout << "with 2147483648" << std::endl;
 	for ( int i=0; i<10; ++i)
 		std::cout << randomizer->frand() << std::endl; // zeroes all the time
+
+	// correctness test:
+	randomizer->reset(1);
+	for ( int n=1; n<10000; ++n)
+	{
+		randomizer->rand();
+	}
+	std::cout << "should be: 1043618064  " << randomizer->rand() << std::endl; // zeroes all the time
+	
 	
 	return 0;
 }
