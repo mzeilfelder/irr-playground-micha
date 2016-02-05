35bb6ea8ed8243108990ccd3d9eacc2a3a22b22f
Michael Zeilfelder <info@michaelzeilfelder.de>
2016-02-05 13:14:22 +0100

    Simplify code.

--- textscenenode_splitscreen.cpp
+++ textscenenode_splitscreen.cpp
@@ -1,5 +1,11 @@
-﻿#include <irrlicht.h>
-#include "driverChoice.h"
+﻿// Code is under the zlib license (same as Irrlicht)
+// Written by mybiandou based on Irrlicht example.
+// Simplified by Michael Zeilfelder
+// 
+// Showing a bug in textscenenodes when used in splitscreen mode
+// Also seems to have a memory leak with current svn trunk of Irrlicht
+
+#include <irrlicht.h>
  
 #ifdef _MSC_VER
 #pragma comment(lib, "Irrlicht.lib")
@@ -11,9 +17,7 @@ using namespace video;
 using namespace scene;
  
 bool SplitScreen=true;
- 
-//cameras
-ICameraSceneNode *camera[4]={0,0,0,0};
+ICameraSceneNode *camera[2]={0,0};
 
 class MyEventReceiver : public IEventReceiver
 {
@@ -27,9 +31,9 @@ class MyEventReceiver : public IEventReceiver
                 SplitScreen = !SplitScreen;
                 return true;
             }
-            //Send all other events to camera4
-            if (camera[3])
-                return camera[3]->OnEvent(event);
+            //Send all other events to last camera
+            if (camera[1])
+                return camera[1]->OnEvent(event);
             return false;
         }
 };
@@ -37,7 +41,8 @@ class MyEventReceiver : public IEventReceiver
 int main()
 {
     MyEventReceiver receiver;
-	video::E_DRIVER_TYPE driverType=video::EDT_OPENGL;
+	//video::E_DRIVER_TYPE driverType=video::EDT_OPENGL;
+	video::E_DRIVER_TYPE driverType=video::EDT_DIRECT3D9;
 	const int ResX=800;
 	const int ResY=600;
 	IrrlichtDevice *device = createDevice(driverType,
@@ -48,48 +53,38 @@ int main()
  
     ISceneManager *smgr = device->getSceneManager();
     IVideoDriver *driver = device->getVideoDriver();
- 
-	irr::gui::IGUIEnvironment* gui = device->getGUIEnvironment();
+ 	irr::gui::IGUIEnvironment* gui = device->getGUIEnvironment();
  
     //add text node
     irr::gui::IGUIFont* font = gui->getBuiltInFont();
     ITextSceneNode *txt=smgr->addTextSceneNode(font,L"Irrlicht",irr::video::SColor(100,255,255,255),0,vector3df(0,0,0),-1);
  
-    // Create 3 fixed and one user-controlled cameras
-    irr::f32 zFar = 300.f;
-    camera[0] = smgr->addCameraSceneNode(0, vector3df(zFar,0,0), vector3df(0,0,0));
-    camera[1] = smgr->addCameraSceneNode(0, vector3df(0,zFar,0), vector3df(0,0,0));
-    camera[2] = smgr->addCameraSceneNode(0, vector3df(0,0,zFar), vector3df(0,0,0));
-    camera[3] = smgr->addCameraSceneNodeFPS();
-    if (camera[3])
-        camera[3]->setPosition(core::vector3df(-50,0,-50));
+	// create cams
+    camera[0] = smgr->addCameraSceneNode(0, vector3df(300.f,0,0), vector3df(0,0,0));
+    camera[1] = smgr->addCameraSceneNodeFPS();
+	camera[1]->setPosition(core::vector3df(-50,0,-50));
    
-    device->getCursorControl()->setVisible(false);	    //Hide mouse
-
     while(device->run())
     {
         //Set the viewpoint to the whole screen and begin scene
         driver->setViewPort(rect<s32>(0,0,ResX,ResY));
         driver->beginScene(true,true,SColor(255,100,100,100));
-        //If SplitScreen is used
+
         if (SplitScreen)
         {
-            //Activate camera1
-            smgr->setActiveCamera(camera[0]);
             //Set viewpoint to the first quarter (left top)
-            driver->setViewPort(rect<s32>(0,0,ResX,ResY/2));
+            smgr->setActiveCamera(camera[0]);
+			driver->setViewPort(rect<s32>(0,0,ResX,ResY/2));
             smgr->drawAll();
  
             //Set viewport the last quarter (right bottom)
             driver->setViewPort(rect<s32>(0,ResY/2,ResX,ResY));
         }
-        //Activate camera4
-        smgr->setActiveCamera(camera[3]);
-        //Draw scene
+        smgr->setActiveCamera(camera[1]);
         smgr->drawAll();
         driver->endScene();
     }
-    //Delete device
+	device->closeDevice();
     device->drop();
     return 0;
 }
