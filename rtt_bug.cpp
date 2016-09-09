db32f9428214474eea3ab99f8c4645ff0c57a047
Michael Zeilfelder <Michael Zeilfelder@localhost>
2016-09-09 17:48:44 +0200

    Added docu.

--- rtt_bug.cpp
+++ rtt_bug.cpp
@@ -1,3 +1,5 @@
+// TEST to debug a problem with rtt and d3d9
+
 #include <irrlicht.h>
 
 using namespace irr;
@@ -6,6 +8,7 @@ using namespace irr;
 #pragma comment(lib, "Irrlicht.lib")
 #endif
 
+// Create a rtt, draw something into it and copy resulting image to a newly created texture
 irr::video::ITexture* drawIntoTexture(irr::video::IVideoDriver * driver)
 {
 	irr::core::dimension2d<irr::u32> texdim(64, 64);
@@ -21,7 +24,8 @@ irr::video::ITexture* drawIntoTexture(irr::video::IVideoDriver * driver)
 	// reset rt
 	driver->setRenderTarget((irr::video::ITexture *)nullptr, irr::video::ECBF_COLOR|irr::video::ECBF_DEPTH);
 
-	// don't use rt directly as rtt's are reset on resize
+	// Don't use rt directly as rtt's are reset on resize
+	// But that part doesn't matter - the rtt is already getting a black texture (have debugged memory directly)
 	irr::video::IImage * img = driver->createImage (rtTex, irr::core::position2di(0,0), rtTex->getSize());
 	driver->removeTexture(rtTex);
 	irr::video::ITexture*  result = driver->addTexture("texture", img);
@@ -50,15 +54,18 @@ int main()
 	test->setPosition(core::vector3df(-10,0,-10));
 	test->setMaterialFlag(video::EMF_LIGHTING, false); // disable dynamic lighting
 
-#if 1	// have to draw anything once to get rtt working on D3D9 
+#if 1	// BUG: when set to 0 the rtt no longer works
+	// We have to draw something once to get rtt working on D3D9 
 	driver->beginScene(true, true, irr::video::SColor(0x00FFFFFF));
 	driver->draw3DLine( core::vector3df(0,0,0), core::vector3df(1,1,1));	// 2d or 3d doesn't matter
 	//driver->draw2DLine( irr::core::vector2di(0,0), irr::core::vector2di(10,10), irr::video::SColor(255,127,127,255));
 	driver->endScene();
 #endif
 
+	// set cube texture to texture created from rtt
 	test->setMaterialTexture(0, drawIntoTexture(driver));
 
+	// just draw the cube
 	while(device->run())
 	{
 		driver->beginScene(video::ECBF_COLOR | video::ECBF_DEPTH, video::SColor(0));
