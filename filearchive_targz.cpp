348b80353bb3fab0e48faf496e71b689df8e8759
Michael Zeilfelder <info@michaelzeilfelder.de>
2017-01-23 23:41:30 +0100

    Improve archive test slightly. Rename as it's really only about tar.gz loading.

--- filearchive_targz.cpp
+++ filearchive_targz.cpp
@@ -1,6 +1,7 @@
 // Code is under the zlib license (same as Irrlicht)
 // Written by Michael Zeilfelder
-// Testing file archive functions
+//
+// Loading tar.gz archives
 
 #include <irrlicht.h>
 #include <iostream>
@@ -15,10 +16,13 @@ int main()
 		return 1; // could not create selected driver.
 
 
+	// get all files in some folder
     io::IFileSystem* fs = device->getFileSystem();
     fs->changeWorkingDirectoryTo("my_media");
     io::IFileList *fileList = fs->createFileList();
-    fs->setFileListSystem(io::FILESYSTEM_NATIVE);
+    fs->setFileListSystem(io::FILESYSTEM_NATIVE);	// not necessary here as it's default, but never hurts to ensure we got correct fs
+
+    // open all .tar.gz archives in that folder
     for (u32 i = 0; i < fileList->getFileCount(); ++i)
     {
         io::path s = fileList->getFullFileName(i);
@@ -47,18 +51,13 @@ int main()
         }
     }
 
+    // Showing files in all archives
     fs->setFileListSystem(io::FILESYSTEM_VIRTUAL);
     fileList = fs->createFileList();
     for (u32 j = 0; j < fileList->getFileCount(); j++)
     {
         io::path s = fileList->getFullFileName(j);
         std::cout << s.c_str() << "\n";;
-
-        if (s.find(".tgz") >= 0 || s.find(".tar.gz") >= 0)
-        {
-        	std::cout << "loading file: " << s.c_str() << "\n";
-			fs->addFileArchive(s.c_str(), false, false, io::EFAT_GZIP);
-        }
     }
 
     return 0;
