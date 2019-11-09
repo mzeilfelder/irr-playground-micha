5a91ce03751c45afb2cdb035742d854675a74d41
Michael Zeilfelder <info@michaelzeilfelder.de>
2019-11-09 15:10:58 +0100

    Add "Shift" state to key_repeat_test

--- key_repeat_test.cpp
+++ key_repeat_test.cpp
@@ -1,17 +1,12 @@
 // Code is under the zlib license (same as Irrlicht)
 // Written by Michael Zeilfelder
-// 
+//
 // Checking key-repeat behavior in Irrlicht.
 
 #include <irrlicht.h>
 #include <iostream>
 
 using namespace irr;
-using namespace core;
-using namespace scene;
-using namespace video;
-using namespace io;
-using namespace gui;
 
 #ifdef _IRR_WINDOWS_
 #pragma comment(lib, "Irrlicht.lib")
@@ -21,40 +16,38 @@ class MyEventReceiver : public IEventReceiver
 {
 	bool OnEvent( const SEvent &event )
 	{
-		if (event.EventType == EET_KEY_INPUT_EVENT) 
+		if (event.EventType == EET_KEY_INPUT_EVENT)
 		{
 			if( event.KeyInput.PressedDown )
 			{
-				std::cout << "PressedDown:" << event.KeyInput.Key << std::endl;
+				std::cout << "PressedDown:" << event.KeyInput.Key <<  " shift:" <<  event.KeyInput.Shift << std::endl;
 			}
 			else if( !event.KeyInput.PressedDown )
 			{
-				std::cout << "!PressedDown:" << event.KeyInput.Key  << std::endl;
+				std::cout << "!PressedDown:" << event.KeyInput.Key << " shift:" <<  event.KeyInput.Shift<< std::endl;
 			}
 		}
-	
+
 		return false;
 	}
 };
 
 int main()
 {
-	video::E_DRIVER_TYPE driverType = video::EDT_OPENGL;
-	IrrlichtDevice * device = createDevice(driverType, core::dimension2d<u32>(640, 480));
+	SIrrlichtCreationParameters params;
+	params.WindowSize = core::dimension2d<u32>(640, 480);
+	params.DriverType = video::EDT_OPENGL;
+	params.DeviceType = EIDT_SDL;
+	IrrlichtDevice * device = createDeviceEx(params);
 	if (device == 0)
 		return 1; // could not create selected driver.
 
 	video::IVideoDriver* driver = device->getVideoDriver();
 	MyEventReceiver receiver;
 	device->setEventReceiver(&receiver);
-	
+
 	while(device->run() && driver)
 	{
-		if (device->isWindowActive())
-		{
-			//			device->sleep(1000);
-			//
-		}
 	}
 
 	device->drop();
