02dd7c87953048b553e377c8cca1d1aa05c43f90
Michael Zeilfelder <Michael Zeilfelder@localhost>
2019-12-13 17:45:45 +0100

    Update shadow-test with new infos

--- shadow_volume.cpp
+++ shadow_volume.cpp
@@ -2,6 +2,10 @@
 // Written by Michael Zeilfelder
 // 
 // A simpler test-case for debugging our shadow volume troubles.
+// Still fails (somewhat depending on driver) with:
+// - zfail = false
+// - infinity larger than far value
+
 
 #include <irrlicht.h>
 #include <iostream>
@@ -54,66 +58,23 @@ int main()
 	node->setMaterialType(video::EMT_TRANSPARENT_ADD_COLOR);
 	node->setMaterialTexture(0, driver->getTexture("../../media/particlewhite.bmp"));
 
-/*	// volumetric light
-	scene::IVolumeLightSceneNode * n = smgr->addVolumeLightSceneNode(0, -1,
-				32,                              // Subdivisions on U axis
-				32,                              // Subdivisions on V axis
-				video::SColor(0, 255, 255, 255), // foot color
-				video::SColor(0, 0, 0, 0));      // tail color
 
-	if (n)
-	{
-		n->setScale(core::vector3df(56.0f, 56.0f, 56.0f));
-		n->setPosition(core::vector3df(-120,50,40));
-
-		// load textures for animation
-		core::array<video::ITexture*> textures;
-		for (s32 g=7; g > 0; --g)
-		{
-			core::stringc tmp;
-			tmp = "../../media/portal";
-			tmp += g;
-			tmp += ".bmp";
-			video::ITexture* t = driver->getTexture( tmp.c_str() );
-			textures.push_back(t);
-		}
-
-		// create texture animator
-		scene::ISceneNodeAnimator* glow = smgr->createTextureAnimator(textures, 150);
-		n->addAnimator(glow);
-		glow->drop();
-	}
-	*/
-
-	const bool zfail = true;
-	const f32 infinity = 10000.f;
 	smgr->setShadowColor(video::SColor(150,0,255,0));
 
-	// add animated character
-//	mesh = smgr->getMesh("../../media/sydney.md2");
-//	scene::IAnimatedMeshSceneNode*	anode = smgr->addAnimatedMeshSceneNode(mesh);
-//	anode->setPosition(core::vector3df(-50,70,-60));
-//	anode->setAnimationSpeed(0);
-//	anode->setCurrentFrame(280);
-//
-//	// add shadow
-//	scene::IShadowVolumeSceneNode* shadow = anode->addShadowVolumeSceneNode(0, -1, zfail, infinity);
-////	shadow->setDebugDataVisible(scene::EDS_MESH_WIRE_OVERLAY);
-//
-//	// make the model a little bit bigger and normalize its normals
-//	// because of the scaling, for correct lighting
-//	anode->setScale(core::vector3df(3,3,3));
-//	anode->setMaterialFlag(video::EMF_NORMALIZE_NORMALS, true);
-
 	// second character
-//	scene::IMesh * cube = smgr->getGeometryCreator()->createCubeMesh( core::vector3df(50.f, 50.f, 50.f) );
+	const bool zfail = true;
+	const f32 infinity = 1000.f;
+
+	scene::IMesh * cube = smgr->getGeometryCreator()->createCubeMesh( core::vector3df(50.f, 50.f, 50.f) );
 //	scene::IMesh * cube = smgr->getGeometryCreator()->createSphereMesh( 50.f );
-	scene::IMesh * cube = smgr->getGeometryCreator()->createArrowMesh( 2, 2, 50.f, 20, 15, 20);
-	cube->getMeshBuffer(0)->getMaterial().BackfaceCulling = false;
-	cube->getMeshBuffer(0)->getMaterial().Wireframe = true;
+//	scene::IMesh * cube = smgr->getGeometryCreator()->createArrowMesh( 2, 2, 50.f, 20, 15, 20);
+	//cube->getMeshBuffer(0)->getMaterial().BackfaceCulling = false;
+	//cube->getMeshBuffer(0)->getMaterial().Wireframe = true;
 	scene::IAnimatedMesh * anicube = smgr->getMeshManipulator()->createAnimatedMesh(cube);
 	cube->drop();
 	scene::IAnimatedMeshSceneNode*	anode2 = smgr->addAnimatedMeshSceneNode(anicube);
+	anode2->setAutomaticCulling(scene::EAC_OFF);
+	anode2->setMaterialFlag(video::EMF_NORMALIZE_NORMALS, true);
 	anicube->drop();
 	anode2->setPosition(core::vector3df(50,70,60));
 	scene::IShadowVolumeSceneNode* shadow2 = anode2->addShadowVolumeSceneNode(0, -1, zfail, infinity);
@@ -121,24 +82,19 @@ int main()
 
 
 	scene::ICameraSceneNode* camera = smgr->addCameraSceneNodeFPS();
-//	scene::ICameraSceneNode* camera = smgr->addCameraSceneNode();
 	camera->setPosition(core::vector3df(-200,200,-100));
 	camera->updateAbsolutePosition();
-	camera->setFarValue( 1000 );	// default is 3000
-//	camera->setTarget(anode->getPosition());
+	camera->setFarValue( 3000 );
 
 	while(device->run())
 	{
 		if (device->isWindowActive())
 		{
-			driver->beginScene(true, true, 0);
+			driver->beginScene(true, true);
 
 			smgr->drawAll();
 
 			driver->endScene();
-
-			//core::stringw str( anode->getFrameNr() );
-			//device->setWindowCaption(str.c_str());
 		}
 	}
 
